741ed5a7911e5a8c50e0412bb0a0b49af7f541f6
Vikram Nathan <vikramn@6829fa18.csail.mit.edu>
2018-10-04 11:12:08 -0400

    change iperf interval to 1s

--- scripts/start_flows.sh
+++ scripts/start_flows.sh
@@ -6,5 +6,5 @@ alg=$3
 bgOut=$4
 ccpOut=$5
 
-iperf -c $MAHIMAHI_BASE -p 4242 -t $dur -i 5 -P $numBgFlows -Z $alg > $bgOut &
-iperf -c $MAHIMAHI_BASE -p 4242 -t $dur -i 5 -Z ccp > $ccpOut
+iperf -c $MAHIMAHI_BASE -p 4242 -t $dur -i 1 -P $numBgFlows -Z $alg > $bgOut &
+iperf -c $MAHIMAHI_BASE -p 4242 -t $dur -i 1 -Z ccp > $ccpOut
