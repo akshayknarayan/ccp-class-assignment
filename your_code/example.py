200e615685ee6ab680bbe0eda5e660e4ca5e9ca1
Vikram Nathan <vikramn@6829fa18.csail.mit.edu>
2018-10-17 17:59:23 -0400

    upload sample python code

---
+++ your_code/example.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import sys
+import portus
+import argparse
+
+parser = argparse.ArgumentParser("Example")
+parser.add_argument("--num-connections",
+        type=int,
+        required=True,
+        help="Number of connections to emulate")
+args = parser.parse_args()
+
+class Reno(portus.GenericCongAvoidBase):
+    def __init__(self, init_cwnd, mss):
+        self.init_cwnd = init_cwnd
+        self.cwnd = init_cwnd
+        self.mss = mss
+        print "Emulating %d connections" % args.num_connections
+
+    def curr_cwnd(self):
+        return self.cwnd
+
+    def set_cwnd(self, cwnd):
+        self.cwnd = cwnd
+
+    def increase(self, m):
+        self.cwnd += self.mss * (m.acked / self.cwnd)
+
+    def reduction(self, m):
+        self.cwnd /= 2.0
+        self.cwnd = max(self.cwnd, self.init_cwnd)
+
+
+portus.start("netlink", Reno, debug=True)
